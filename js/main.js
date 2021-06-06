99156b8d61c3317174dfb91b91f5215eade83d7b
Наташа <natasha.denisencko@yandex.ru>
2021-06-06 14:15:06 +0300

    Исправила ошибки ESLint

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 function getRandomInt (min, max) {
-	min = Math.abs(Math.ceil(min));
-	max = Math.abs(Math.floor(max));
-	if (min > max) {
-		let minNumber = min; 
-		min = max; 
-		max = minNumber;
-	}
-	return Math.round(Math.random() * (max - min) + min);
+  min = Math.abs(Math.ceil(min));
+  max = Math.abs(Math.floor(max));
+  if (min > max) {
+    return getRandomInt(max, min);
+  }
+  return Math.round(Math.random() * (max - min) + min);
 }
 
+getRandomInt (3, 1);
+
 function getRandomFloat (min, max, round) {
-	min = Math.abs(Math.ceil(min));
-	max = Math.abs(Math.floor(max));
-	if (min > max) {
-		let minNumber = min; 
-		min = max; 
-		max = minNumber;
-	}
-	return (Math.random() * (max - min) + min).toFixed(round);
+  min = Math.abs(Math.ceil(min));
+  max = Math.abs(Math.floor(max));
+  if (min > max) {
+    return getRandomInt(max, min);
+  }
+  return (Math.random() * (max - min) + min).toFixed(round);
 }
+
+getRandomFloat (1, 5, 2);
