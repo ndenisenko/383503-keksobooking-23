f2f067d3d72d2cc00ab8cf595ce7870aa3707ea2
Наташа <natasha.denisencko@yandex.ru>
2021-07-03 19:57:45 +0300

    Добавила проверку на пустоту для одного поля

--- js/createCard.js
+++ js/createCard.js
@@ -12,39 +12,35 @@ const itemTemplate = document.querySelector('#card').content;
 const popup = itemTemplate.querySelector('.popup');
 const mapCanvas = document.querySelector('#map-canvas');
 
-/*const checkOnEmptiness = function (array) {
-  array.forEach ((item) => {
-    if (! item.textContent) {
-      item.style.display = 'none';
-    } else {
-      item.style.display = 'block';
-    }
-  })
-};*/
-
 const generateSimilarElements = function () {
-  // const arraytoCheck = [];
 
   // Формирование одного объявления
-  for (let i = 0; i < 1; i++) {
+  for (let index = 0; index < 1; index++) {
     const element = popup.cloneNode(true);
-    const itemOffer = list[i]['offer'];
-    element.querySelector('.popup__title').textContent = itemOffer['title'];
+    const itemOffer = list[index]['offer'];
+
+    // Конструкция рабочая, но множить не хочу, придумаю функцию
+    if (itemOffer['title']) {
+      element.querySelector('.popup__title').textContent = itemOffer['title'];
+    } else {
+      element.querySelector('.popup__title').style.display = 'none';
+    }
+
     element.querySelector('.popup__text--address').textContent = itemOffer['address'];
     element.querySelector('.popup__text--price').textContent = `${itemOffer['price'].toString()  } ₽/ночь`;
     element.querySelector('.popup__type').textContent = apartmentTypes[itemOffer['type']];
     element.querySelector('.popup__text--capacity').textContent = `${itemOffer['rooms'].toString()  } комнаты для ${  itemOffer['guests'].toString()  } гостей`;
     element.querySelector('.popup__text--time').textContent = `Заезд после ${  itemOffer['checkin'].toString()  }, выезд после ${  itemOffer['checkout'].toString()}`;
+    element.querySelector('.popup__description').textContent = itemOffer['description'];
 
     const photoList = element.querySelector('.popup__photos');
     const photo = photoList.querySelector('.popup__photo');
     const photosOffer = itemOffer['photos'];
-
     if (photosOffer[0]){
       photo.src = itemOffer['photos'][0];
-      for (let j = 1; j < photosOffer.length; j++) {
+      for (let jIndex = 1; jIndex < photosOffer.length; jIndex++) {
         const photoCloned = photo.cloneNode(true);
-        photoCloned.src = itemOffer['photos'][j];
+        photoCloned.src = itemOffer['photos'][jIndex];
         photoList.appendChild(photoCloned);
       }
     } else {
@@ -61,9 +57,7 @@ const generateSimilarElements = function () {
       }
     });
 
-    element.querySelector('.popup__description').textContent = itemOffer['description'];
-    element.querySelector('.popup__avatar').src = list[i]['author']['avatar'];
-
+    element.querySelector('.popup__avatar').src = list[index]['author']['avatar'];
     mapCanvas.appendChild(element);
   }
 };
