e17d7ae74458874046087153ebcd40538bc5fa23
Наташа <natasha.denisencko@yandex.ru>
2021-07-18 16:45:03 +0300

    Добавила активное/неактивное состония

--- js/form-modal.js
+++ js/form-modal.js
@@ -47,7 +47,39 @@ const userCapacityInputHandler = function () {
   return userRoomsCapacityHandler(userRoomsInput);
 };
 
+const form = document.querySelector('.ad-form');
+const formItemsList = form.querySelectorAll('fieldset');
+const map = document.querySelector('.map__filters');
+const mapFilterList = map.querySelectorAll('select');
+
+const blockMap = function () {
+  formItemsList.forEach((item) => item.setAttribute('disabled', ''));
+  form.classList.add('ad-form--disabled');
+
+  map.classList.add('.map__filters--disabled');
+  mapFilterList.forEach((item) => item.setAttribute('disabled', ''));
+
+  // const mapFeautures = map.querySelector('fieldset').children;
+  // for (let i = 0; i < mapFeautures.length; i++) {
+  //   mapFeautures[i].setAttribute('disabled', '');
+  //   console.log(mapFeautures[i]);
+  // }
+};
+
+const unblockMap = function () {
+  formItemsList.forEach((item) => item.removeAttribute('disabled', ''));
+  form.classList.remove('ad-form--disabled');
+
+  map.classList.remove('.map__filters--disabled');
+  mapFilterList.forEach((item) => item.removeAttribute('disabled', ''));
+};
+
+unblockMap();
+blockMap();
+
 userNameInput.addEventListener('input', userNameInputHandler);
 userPriceInput.addEventListener('input', userPriceInputHandler);
 userRoomsInput.addEventListener('change', userRoomsInputHandler);
 userCapacityInput.addEventListener('change', userCapacityInputHandler);
+
+
