11acde51f597a3405f8d655f5833ec71fed4d8b2
Наташа <natasha.denisencko@yandex.ru>
2021-07-22 21:16:10 +0300

    Добавила валидацию полей время заезда/выезда, стоимость, обработчики

--- js/form-modal.js
+++ js/form-modal.js
@@ -1,11 +1,68 @@
 const MIN_NAME_LENGTH = 30;
 const MAX_NAME_LENGTH = 100;
 const MAX_PRICE = 1000000;
+let priceValue = 0;
 
 const userNameInput = document.querySelector('input[name="title"]');
 const userPriceInput = document.querySelector('input[name="price"]');
 const userRoomsInput = document.querySelector('select[name="rooms"]');
 const userCapacityInput = document.querySelector('select[name="capacity"]');
+const userApartTypeInput = document.querySelector('select[name="type"]');
+const userTimeInInput = document.querySelector('select[name="timein"]');
+const userTimeOutInput = document.querySelector('select[name="timeout"]');
+
+const timeInOutHandler = function (userTime, autoTime) {
+  switch (userTime.value) {
+    case '12:00':
+      autoTime.value = '12:00';
+      break;
+    case '13:00':
+      autoTime.value = '13:00';
+      break;
+    case '14:00':
+      autoTime.value = '14:00';
+      break;
+    default:
+  }
+};
+
+const userTimeInInputHandler = function () {
+  return timeInOutHandler(userTimeInInput, userTimeOutInput);
+};
+
+const userTimeOutInputHandler = function () {
+  return timeInOutHandler(userTimeOutInput, userTimeInInput);
+};
+
+const userApartTypeInputHandler = function () {
+  //
+  switch (userApartTypeInput.value) {
+    case 'bungalow':
+      priceValue = 0;
+      break;
+    case 'flat':
+      priceValue = 1000;
+      break;
+    case 'hotel':
+      priceValue = 3000;
+      break;
+    case 'palace':
+      priceValue = 10000;
+      break;
+    case 'house':
+      priceValue = 5000;
+      break;
+    default:
+  }
+  userPriceInput.setAttribute('placeholder', priceValue);
+
+  if (userPriceInput.value < userPriceInput.getAttribute('placeholder')) {
+    userPriceInput.setCustomValidity(`Цена должна быть больше ${ userPriceInput.getAttribute('placeholder')} руб.`);
+  } else {
+    userPriceInput.setCustomValidity('');
+  }
+  userPriceInput.reportValidity();
+};
 
 const userNameInputHandler = function () {
   const valueLength = userNameInput.value.length;
@@ -22,7 +79,9 @@ const userNameInputHandler = function () {
 
 const userPriceInputHandler = function () {
   if (userPriceInput.value > MAX_PRICE) {
-    userPriceInput.setCustomValidity(`Цена должнабыть меньше ${ MAX_PRICE} руб.`);
+    userPriceInput.setCustomValidity(`Цена должна быть меньше ${ MAX_PRICE} руб.`);
+  } else if (userPriceInput.value < userPriceInput.getAttribute('placeholder')) {
+    userPriceInput.setCustomValidity(`Цена должна быть больше ${ userPriceInput.getAttribute('placeholder')} руб.`);
   } else {
     userPriceInput.setCustomValidity('');
   }
@@ -68,12 +127,13 @@ const unblockMap = function () {
   mapFilterList.forEach((item) => item.removeAttribute('disabled'));
 };
 
-unblockMap();
-blockMap();
 
 userNameInput.addEventListener('input', userNameInputHandler);
 userPriceInput.addEventListener('input', userPriceInputHandler);
 userRoomsInput.addEventListener('change', userRoomsInputHandler);
 userCapacityInput.addEventListener('change', userCapacityInputHandler);
+userApartTypeInput.addEventListener('change', userApartTypeInputHandler);
+userTimeInInput.addEventListener('change', userTimeInInputHandler);
+userTimeOutInput.addEventListener('change', userTimeOutInputHandler);
 
-
+export {blockMap, unblockMap};
